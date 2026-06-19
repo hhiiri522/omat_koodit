f364913a710ef707cda7f6b9628d416901c2c3da
Hiiri522 <hhiiri522@gmail.com>
2026-06-19 09:41:47 +0300

    Create joulupukkikysely.py

---
+++ Python-kurssi 2022-2023/joulupukkikysely.py
@@ -0,0 +1,8 @@
+kysymys = input("Oletko ollut kiltti tänä vuonna? ")
+
+if kysymys == "kyllä":
+  print("Saat paljon lahjoja.")
+elif kysymys == "ei":
+  print("Saat lahjaksi risuja.")
+elif kysymys == "en tiedä":
+  print("Emme voi arvioida lahjojesi määrää.")
