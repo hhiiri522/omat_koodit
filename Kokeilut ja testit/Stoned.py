48b00c073999f54b7e6b8667f0da81e66c69c18b
Hiiri522 <hhiiri522@gmail.com>
2026-06-20 11:29:55 +0300

    Latasin Stoned.py-tiedoston
    Stoned on ikivanha tietokonevirus. Se on kuitenkin visuaalisesti erittäin yksinkertainen. Kirjoitin keväällä 2023 Python-koodin, jonka lopputulos näyttää käytännössä samalta kuin Stoned-virus.

---
+++ Kokeilut ja testit/Stoned.py
@@ -0,0 +1,15 @@
+#!/bin/python3#
+import time
+virke = "Your PC is now Stoned!"
+print(virke)
+time.sleep(1)
+print(virke)
+time.sleep(1)
+print(virke)
+time.sleep(1)
+print(virke)
+time.sleep(1)
+print(virke)
+time.sleep(1)
+print(virke)
+time.sleep(1)
