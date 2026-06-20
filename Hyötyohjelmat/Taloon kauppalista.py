dfda422848489d31d32e9314dbb5e7f2aae6eaa9
Hiiri522 <hhiiri522@gmail.com>
2026-06-20 10:06:37 +0300

    Latasin Taloon kauppalista.py -tiedoston

---
+++ Hyötyohjelmat/Taloon kauppalista.py
@@ -0,0 +1,15 @@
+#!/bin/python3 #
+kauppalista = ["puhdistuspyyhkeet", "käsidesi"]
+print(kauppalista)
+
+kauppalista.append("juomapulloteline")
+print(kauppalista)
+
+kauppalista.append("pyyhe")
+print(kauppalista)
+
+kauppalista.remove("käsidesi")
+print(kauppalista)
+
+kauppalista.remove("juomapulloteline")
+print(kauppalista)
